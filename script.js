15c9e45a5e8d45aed978fcf24afc4615119a8d6e
temponius <skorpiio79@gmail.com>
2025-11-12 18:58:16 -0800

    Reset code

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ const grid = document.querySelector(".sketchingGrid");
 function createGrid(cells) {
   for (let i = 0; i < cells; i++) {
     const cell = document.createElement("div");
-    cell.className = "cellClass";
+    cell.classList = "cellClass";
+    cell.addEventListener("mouseover", function () {
+      cell.classList.add("touched");
+    });
+    console.log(cell.classList);
     grid.appendChild(cell);
   }
 }
 
+function resetGrid() {
+  let targetCells = document.getElementsByClassName("cellClass");
+  console.log("Hello!");
+  targetCells.forEach((div) => {
+    div.classList.remove("touched");
+  });
+}
 createGrid(cells);
